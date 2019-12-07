c9d66166d75dd5c11fc65329b658cb0a9c18eb5a
Henry <hcheung@umass.edu>
2019-12-07 18:53:30 -0500

    Rename Page

--- app/src/main/java/com/example/duynguyen/studyapp/Page2.java
+++ app/src/main/java/com/example/duynguyen/studyapp/Page2.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.example.duynguyen.studyapp.R;
 
-public class Main2Activity extends AppCompatActivity {
+public class Page2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
