2c4620865e4be3c00804b87348cfe319fff36b8e
dgiang413 <33362378+dgiang413@users.noreply.github.com>
2019-12-07 18:08:52 -0500

    Delete extra duy nguyens

--- app/src/main/java/com/example/duynguyen/studyapp/Main2Activity.java
+++ app/src/main/java/com/example/duynguyen/studyapp/Main2Activity.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.duynguyen.studyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/example/duynguyen/studyapp/MainActivity.java
+++ app/src/main/java/com/example/duynguyen/studyapp/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.Main2Activity;
-import com.example.duynguyen.studyapp.R;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
