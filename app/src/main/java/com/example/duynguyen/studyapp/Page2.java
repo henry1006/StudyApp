c02fca2c131612859f532440e394b6f2f17bc6e3
Duy Nguyen <duyn1269@gmail.com>
2019-12-08 00:39:52 -0500

    Finish

--- app/src/main/java/com/example/duynguyen/studyapp/Page2.java
+++ app/src/main/java/com/example/duynguyen/studyapp/Page2.java
@@ -1,36 +1,41 @@
 package com.example.duynguyen.studyapp;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import com.example.duynguyen.studyapp.R;
+import java.util.ArrayList;
 
 public class Page2 extends AppCompatActivity {
 
-    private Context mContext;
-    private Activity mActivity;
-
-    private RelativeLayout mRelativeLayout;
-    private TextView mTextView;
-    private Toolbar mToolbar;
-
-    private boolean mBold = false;
-    private boolean mItalic = false;
+    private RecyclerView cities;
+    private RecyclerView.Adapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ArrayList<City> cities = initCities();
+
+        this.cities = (RecyclerView) findViewById(R.id.cities);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        this.cities.setLayoutManager(mLayoutManager);
+
+        adapter = new CityAdapter(cities);
+        this.cities.setAdapter(adapter);
+    }
+
+    private ArrayList<City> initCities() {
+        ArrayList<City> list = new ArrayList<>();
+
+        list.add(new City("Dubois LLC", "Loud. Printer. Computer. Toilet", "https://www.library.umass.edu/assets/Uploads/_resampled/ResizedImageWzYwMCwyNjFd/gradwide800.jpg"));
+        list.add(new City("Dubois Cafe", "Loud. Food. Drink. Bright Light", "https://umassdining.com/sites/default/files/images/procrastination_station%20_resized.jpg"));
+        list.add(new City("Dubois 23rd Floor", "Quiet. Dim Light. Outlet. Study Materials", "https://amherstwire.com/wp-content/uploads/2019/03/6OqAdPo2RyKaZ7lRp0i49w_thumb_6a23.jpg"));
+
+
+        return list;
     }
 }
